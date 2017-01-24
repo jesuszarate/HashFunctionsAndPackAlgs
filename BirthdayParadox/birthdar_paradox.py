95940fb26eb69bd066dd3dd472b6ad720a7a657b
Jesus Zarate <zarate.jesus@outlook.com>
2017-01-23 19:15:50 -0700

    Showing plot cdf

--- BirthdayParadox/birthdar_paradox.py
+++ BirthdayParadox/birthdar_paradox.py
@@ -10,15 +10,12 @@ def randTrials(start, end, k):
     while 1:
         v = random.randint(start, n)
         if v in bdays:
-            bdays[v] = True
-            print "\n" + str(v)
-            print k
+            return k
             break
+
         bdays[v] = True
         k += 1
 
-
-    print bdays.keys()
-
 for i in range(1, 300):
-    randTrials(start, n, k)
+    #bdays = {}
+    print str(i/300.0) + "\t" + str(randTrials(start, n, k))
